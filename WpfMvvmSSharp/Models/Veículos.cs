8d9de1731ec6eabd2105ede0655b668b7528220f
Fabiano Rocha <31971552+dev-fabianorocha@users.noreply.github.com>
2023-11-19 20:28:57 -0300

    Initial commit

---
+++ WpfMvvmSSharp/Models/Veículos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvmSSharp.Models
+{
+    public class Veículos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Modelo { get; set; }
+        public short Ano { get; set; }
+
+        public List<Veículos> ObterVeiculos()
+        {
+            List<Veículos> listaVeículos = new List<Veículos>()
+            {
+                new Veículos() { Id = 1, Nome = "FORD FIESTA", Modelo = "1.0 MPI PERSONNALITÉ SEDAN 4P",
+                    Ano =2005 },
+                new Veículos() { Id = 2, Nome = "HONDA CRV", Modelo = "2.0 LX 4X2 16V 4P",
+                    Ano =2018 },
+                new Veículos() { Id = 3, Nome = "LAND ROVER DISCOVERY 4", Modelo = "3.0 SE 4X4 V6 24V BI-TURBO 4P",
+                    Ano =2021 }
+            };
+
+            return listaVeículos;
+        }
+
+        public List<Veículos> ObterVeículos(string filtro)
+        {
+            var listaFiltrada = (from c in ObterVeiculos()
+                                where c.Nome.Contains(filtro.ToUpper())
+                                select c).ToList();
+
+            return listaFiltrada;
+        }
+    }
+}
---
+++ WpfMvvmSSharp/ViewModels/VeículosViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfMvvmSSharp.Models;
+using WpfMvvmSSharp.Services;
+
+namespace WpfMvvmSSharp.ViewModels
+{
+    public class VeículosViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        private string _marca;
+        public string Marca 
+        {
+            get => _marca;
+            set
+            {
+                if (_marca != value)
+                {
+                    _marca = value;
+                    NotifyPropertyChanged("Marca");
+                }
+            }
+        }
+
+        public ObservableCollection<Veículos> Veículos { get; set; }
+
+        public ICommand BuscarVeículos { get; set; }
+
+
+
+        public VeículosViewModel()
+        {
+            BuscarVeículos = new RelayCommand(ObterVeículos);
+        } 
+
+        public void ObterVeículos(object obj)
+        {
+            var veículosList = new Veículos().ObterVeículos(Marca);
+            Veículos = new ObservableCollection<Veículos>(veículosList as List<Veículos>);
+            NotifyPropertyChanged("Veículos");
+        }
+
+        public void NotifyPropertyChanged(string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
